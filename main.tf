f3734adb468da53eeddffb1d97f4877a84492923
SudeepaChekuri <98836806+SudeepaChekuri@users.noreply.github.com>
2023-12-04 18:09:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "ap-south-1"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-02a2af70a66af6dfb" # ap-south-1
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
